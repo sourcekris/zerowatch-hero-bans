8959e98f27ca01259f7a43d21557b2f8ca5c8a10
Kris <sourcekris@users.noreply.github.com>
2026-02-20 13:54:57 +1100

    Update server to use environment variable for port

--- server.js
+++ server.js
@@ -81,6 +81,9 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Zerowatch server running on http://localhost:3000');
+// --- UPDATED PORT CONFIGURATION ---
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Zerowatch server running on port ${PORT}`);
 });
